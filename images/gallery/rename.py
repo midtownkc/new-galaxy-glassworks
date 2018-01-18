aa823902c914544a0ddd6edfc79379ef2bc6a2c8
Maxwell Frederickson <maxwellfrederickson@gmail.com>
2018-01-17 22:01:52 -0600

    renamed gallery pics

--- images/gallery/rename.py
+++ images/gallery/rename.py
@@ -4,10 +4,9 @@ path =  os.getcwd()
 files = os.listdir(path)
 i = 0
 
-
-
 for file in files:
-    filename, file_extension = os.path.splitext("%s%s" os.getcwd(), file)
-    if file_extension.lower() == jpg:
+    filename, file_extension = os.path.splitext(path + file)
+    if file_extension.lower() == '.jpg':
       i = i + 1
-      os.rename(file, str(i))
+      new_name = (str(i) + file_extension.lower())
+      os.rename(file, new_name)
